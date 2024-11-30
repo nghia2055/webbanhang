c5a9220dbedbc36ebd0a2bb940595e586c6dd698
nghia <dtn04999@gmail.com>
2024-11-30 10:59:23 +0700

    nghia21321321

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_NEXTJS);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS,PUT, PATCH,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
